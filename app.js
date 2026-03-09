ea492a1e2870dd960202148541666d533499927b
maheen <maheenzuhra@gmail.com>
2026-03-09 22:02:31 +0500

    add login

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-var title = document.getElementById("title");
-var descr = document.getElementById("body");
-var post = document.getElementById("post");
+// var title = document.getElementById("title");
+// var descr = document.getElementById("body");
+// var post = document.getElementById("post");
 
-function sumbitPost(){
-    console.log(title.value , descr.value);
-    post.innerHTML =`<h3>${title.value}</h3>
-<p>${descr.value}</p>`
+// function sumbitPost(){
+//     console.log(title.value , descr.value);
+//     post.innerHTML =`<h3>${title.value}</h3>
+// <p>${descr.value}</p>`
     
+// }
+function authForm(){
+    
+    window.location.href = "index.html";
 }
