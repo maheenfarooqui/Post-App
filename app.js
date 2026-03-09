2d2820d0a67b21246f6cbd4b02c766b7ec3c7dd1
maheen <maheenzuhra@gmail.com>
2026-03-09 13:19:42 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var title = document.getElementById("title");
+var descr = document.getElementById("body");
+var post = document.getElementById("post");
+
+function sumbitPost(){
+    console.log(title.value , descr.value);
+    post.innerHTML =`<h3>${title.value}</h3>
+<p>${descr.value}</p>`
+    
+}
