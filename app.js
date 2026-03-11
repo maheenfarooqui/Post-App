2b626815663c94cae622ea8c40dc572265b927b2
maheen <maheenzuhra@gmail.com>
2026-03-11 23:00:16 +0500

    add edit button logic

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 var title = document.getElementById("title");
 var descr = document.getElementById("body");
 var post = document.getElementById("post");
-var currentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
 var bgImg = document.getElementsByClassName("bgimg");
 var userE = document.getElementById("userE");
 var userP = document.getElementById("userP");
 var userN = document.getElementById("userN");
 var userIcon = document.getElementById("userIcon");
-var cardBg;
-var editPostText= "";
+
+var cardBg = "";
+var editPostText = "";
+var currentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
 
 function authForm() {
   if (userE.value === "" && userE.value === "" && userN.value === "") {
@@ -40,40 +41,6 @@ Swal.fire({
     htmlContainer: "vibenet-content",
   },
 });
-function addClass(src) {
-  cardBg = src;
-  for (var i = 0; i < bgImg.length; i++) {
-    bgImg[i].className = "bgimg";
-  }
-
-  event.target.classList.add("selected");
-}
-function colorChange(likeBtn) {
-  if (likeBtn.innerText === "Like") {
-    likeBtn.style.color = "#22D3EE";
-    likeBtn.innerText = "Liked";
-  } else {
-    likeBtn.style.color = "#6F7A8D";
-    likeBtn.innerText = "Like";
-  }
-}
-function editPost(editBtn){
-  var getEL =editBtn.closest('.cardWraper');
-  var maintitle =getEL.querySelector('h5').innerText.trim();
-  var mainText =getEL.querySelector('p').innerText.trim();
-  title.value = maintitle;
-  descr.value = mainText;
-   editPostText =getEL;
-   console.log(cardBg);
-   
-   
-   
-  }
-  function deletePost(deleteBtn){
-    var getCard = deleteBtn.closest('.cardWraper');
-    getCard.style.display = "none"
-
-  }
 
 function sumbitPost() {
   if (title.value.trim() === "" && descr.value.trim() === "") {
@@ -81,9 +48,11 @@ function sumbitPost() {
       icon: "error",
       title: "Oops...",
       text: "Title and Description required",
-    }); return}
-    if(editPostText === ""){
-post.innerHTML += `<div class="cardWraper">
+    });
+    return;
+  }
+  if (editPostText === "") {
+    post.innerHTML += `<div class="cardWraper">
     <div class="postCard p-4 mb-4 border-0 shadow-sm mb-2" style="background-image: url(${cardBg}); background-attachment: fixed; background-size: cover; background-position: center;">
                 <div class="d-flex align-items-center mb-3">
                 <div class="user-profile-circle me-3 bg-info">${userNAme.charAt(0).toUpperCase()}</div>
@@ -115,30 +84,59 @@ post.innerHTML += `<div class="cardWraper">
               </div>
     <div/>
               `;
-  }else{
-   editPostText.querySelector('h5').innerText= title.value;
-  editPostText.querySelector('p').innerText =descr.value;
-    
-   
-    
+  } else {
+    editPostText.querySelector("h5").innerText = title.value;
+    editPostText.querySelector("p").innerText = descr.value;
+    var mainCard = editPostText.querySelector(".postCard");
+    mainCard.style.backgroundImage = `url(${cardBg})`;
   }
-    
 
-  
   title.value = "";
   descr.value = "";
+  editPostText = "";
 
   removeSelected();
 }
 
+function editPost(editBtn) {
+  var getEL = editBtn.closest(".cardWraper");
+  var maintitle = getEL.querySelector("h5").innerText.trim();
+  var mainText = getEL.querySelector("p").innerText.trim();
+  title.value = maintitle;
+  descr.value = mainText;
+  editPostText = getEL;
+
+  var mainCard = getEL.querySelector(".postCard");
+  var editBg = mainCard.style.backgroundImage;
+  cardBg = editBg;
+}
+
+function deletePost(deleteBtn) {
+  var getCard = deleteBtn.closest(".cardWraper");
+  getCard.style.display = "none";
+}
+function addClass(src) {
+  cardBg = src;
+  for (var i = 0; i < bgImg.length; i++) {
+    bgImg[i].className = "bgimg";
+  }
+
+  event.target.classList.add("selected");
+}
+function colorChange(likeBtn) {
+  if (likeBtn.innerText === "Like") {
+    likeBtn.style.color = "#22D3EE";
+    likeBtn.innerText = "Liked";
+  } else {
+    likeBtn.style.color = "#6F7A8D";
+    likeBtn.innerText = "Like";
+  }
+}
 function removeSelected() {
   for (var i = 0; i < bgImg.length; i++) {
     bgImg[i].className = "bgimg";
   }
 }
-
-
 function logout() {
   window.location.href = "login.html";
 }
-
