fe2d2c403ec32878d19faef057f673645fdc864b
maheen <maheenzuhra@gmail.com>
2026-03-11 13:07:15 +0500

    add edit function

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ var bgImg = document.getElementsByClassName("bgimg");
 var userE = document.getElementById("userE");
 var userP = document.getElementById("userP");
 var userN = document.getElementById("userN");
-var userIcon = document.getElementById("userIcon")
+var userIcon = document.getElementById("userIcon");
 var cardBg;
- 
+var editPostText= "";
+
 function authForm() {
   if (userE.value === "" && userE.value === "" && userN.value === "") {
     Swal.fire({
@@ -29,16 +30,50 @@ Swal.fire({
   icon: "success",
   title: "LogIn Successfully",
   text: "Welcome to VibeNet " + userNAme,
-  color: '#ffffff',
-  background: '#1e293b',
-  showConfirmButton: false, 
-  timer: 3000, 
+  color: "#ffffff",
+  background: "#1e293b",
+  showConfirmButton: false,
+  timer: 3000,
   customClass: {
-    popup: 'vibenet-popup',
-    title: 'vibenet-title',
-    htmlContainer: 'vibenet-content' 
-  }
+    popup: "vibenet-popup",
+    title: "vibenet-title",
+    htmlContainer: "vibenet-content",
+  },
 });
+function addClass(src) {
+  cardBg = src;
+  for (var i = 0; i < bgImg.length; i++) {
+    bgImg[i].className = "bgimg";
+  }
+
+  event.target.classList.add("selected");
+}
+function colorChange(likeBtn) {
+  if (likeBtn.innerText === "Like") {
+    likeBtn.style.color = "#22D3EE";
+    likeBtn.innerText = "Liked";
+  } else {
+    likeBtn.style.color = "#6F7A8D";
+    likeBtn.innerText = "Like";
+  }
+}
+function editPost(editBtn){
+  var getEL =editBtn.closest('.cardWraper');
+  var maintitle =getEL.querySelector('h5').innerText.trim();
+  var mainText =getEL.querySelector('p').innerText.trim();
+  title.value = maintitle;
+  descr.value = mainText;
+   editPostText =getEL;
+   console.log(cardBg);
+   
+   
+   
+  }
+  function deletePost(deleteBtn){
+    var getCard = deleteBtn.closest('.cardWraper');
+    getCard.style.display = "none"
+
+  }
 
 function sumbitPost() {
   if (title.value.trim() === "" && descr.value.trim() === "") {
@@ -46,12 +81,10 @@ function sumbitPost() {
       icon: "error",
       title: "Oops...",
       text: "Title and Description required",
-    });
-  } else {
-    
-   
-    post.innerHTML += `<div class="postCard p-4 mb-4 border-0 shadow-sm mb-2" style="background-image: url(${cardBg}); background-attachment: fixed; background-size: cover; background-position: center;">
-              
+    }); return}
+    if(editPostText === ""){
+post.innerHTML += `<div class="cardWraper">
+    <div class="postCard p-4 mb-4 border-0 shadow-sm mb-2" style="background-image: url(${cardBg}); background-attachment: fixed; background-size: cover; background-position: center;">
                 <div class="d-flex align-items-center mb-3">
                 <div class="user-profile-circle me-3 bg-info">${userNAme.charAt(0).toUpperCase()}</div>
                 <div>
@@ -63,8 +96,6 @@ function sumbitPost() {
               <p class="text-light opacity-75">
                ${descr.value}
               </p>
-            
-              
             </div>
             <div class="py-3 border-top border-secondary d-flex gap-4">
                 <button
@@ -82,35 +113,32 @@ function sumbitPost() {
             </button>
                 </div>
               </div>
+    <div/>
               `;
+  }else{
+   editPostText.querySelector('h5').innerText= title.value;
+  editPostText.querySelector('p').innerText =descr.value;
+    
+   
+    
   }
+    
+
+  
   title.value = "";
   descr.value = "";
+
   removeSelected();
 }
-function addClass(src) {
-  cardBg = src;
-  for (var i = 0; i < bgImg.length; i++) {
-    bgImg[i].className = "bgimg";
-  }
 
-  event.target.classList.add("selected");
-}
-
-function logout() {
-  window.location.href = "login.html";
-}
-function colorChange(likeBtn) {
-  if (likeBtn.innerText === "Like") {
-    likeBtn.style.color = "#22D3EE";
-    likeBtn.innerText = "Liked";
-  } else {
-    likeBtn.style.color = "#6F7A8D";
-    likeBtn.innerText = "Like";
-  }
-}
 function removeSelected() {
   for (var i = 0; i < bgImg.length; i++) {
     bgImg[i].className = "bgimg";
   }
 }
+
+
+function logout() {
+  window.location.href = "login.html";
+}
+
