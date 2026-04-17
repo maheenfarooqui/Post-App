a73103f16803e36679c187b3f5cf29a3a613629b
maheen <maheenzuhra@gmail.com>
2026-04-17 09:23:19 +0500

    add local storage

--- app.js
+++ app.js
@@ -6,68 +6,144 @@ var userE = document.getElementById("userE");
 var userL = document.getElementById("userL");
 var userP = document.getElementById("userP");
 var userN = document.getElementById("userN");
-var userIcon = document.getElementById("userIcon");
-
+var icon = document.getElementById("icon");
 var cardBg = "";
 var currentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
 
 function authForm() {
-  if (userE.value === "" && userL.value === "" && userN.value === "") {
+  if (userE.value === "" || userL.value === "" || userN.value === "") {
     Swal.fire({
       icon: "error",
       title: "Oops...",
       text: "Email and password required",
     });
+  }
+  var storage = JSON.parse(localStorage.getItem("userData"));
+
+  if (!storage) {
+    storage = [];
+  }
+
+  var userDetails = {
+    userFName: userN.value,
+    userLName: userL.value,
+    userEmail: userE.value,
+    userPass: userP.value,
+  };
+
+  storage.push(userDetails);
+
+  localStorage.setItem("userData", JSON.stringify(storage));
+
+  Swal.fire({
+    icon: "success",
+    title: "Account create Successfully",
+    text: "Welcome to VibeNet " + userN.value.toUpperCase(),
+    color: "#ffffff",
+    background: "#1e293b",
+    showConfirmButton: false,
+    timer: 3000,
+    customClass: {
+      title: "vibenet-title",
+    },
+  }).then(() => {
+    window.location.href = "login.html";
+  });
+}
+
+function logIn() {
+  var userData = localStorage.getItem("userData");
+  var userInfo = JSON.parse(userData);
+  if (!userData) {
+    Swal.fire({
+      icon: "error",
+      title: "No account found",
+      text: "Please create an account first",
+    });
+    return;
+  }
+  localStorage.setItem("currentUser", JSON.stringify(foundUser));
+  var foundUser = userInfo.find(function (user) {
+    return (
+      user.userFName === userN.value &&
+      user.userEmail === userE.value &&
+      user.userPass === userP.value
+    );
+  });
+
+  if (foundUser) {
+    localStorage.setItem("currentUser", JSON.stringify(foundUser));
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "LogIn Successfully",
+      text: "Welcome to VibeNet " + userN.value.toUpperCase(),
+      color: "#ffffff",
+      background: "#1e293b",
+      showConfirmButton: false,
+      timer: 3000,
+      customClass: {
+        title: "vibenet-title",
+      },
+    }).then(() => {
+      window.location.href = "dashboard.html";
+    });
   } else {
-    localStorage.setItem("userName", userN.value);
-    localStorage.setItem("userLname", userL.value);
-    window.location.href = "dashboard.html";
+    Swal.fire({
+      icon: "error",
+      title: "Login Failed",
+      text: "Invalid name, email, or password",
+    });
   }
 }
-var userNAme = localStorage.getItem("userName");
-var userLname = localStorage.getItem("userLname");
-userIcon.innerText =
-  userNAme.charAt(0).toUpperCase() + userLname.charAt(0).toUpperCase();
+function showUserIcon() {
+  var currentUser = localStorage.getItem("currentUser");
 
-Swal.fire({
-  position: "top-end",
-  icon: "success",
-  title: "LogIn Successfully",
-  text: "Welcome to VibeNet " + userNAme,
-  color: "#ffffff",
-  background: "#1e293b",
-  showConfirmButton: false,
-  timer: 3000,
-  customClass: {
-   title: "vibenet-title",
-  },
-});
+  if (!currentUser) return;
+
+  var userInfo = JSON.parse(currentUser);
+
+  var icon = document.getElementById("icon");
+
+  if (icon) {
+    icon.innerHTML =
+      userInfo.userFName.charAt(0).toUpperCase() +
+      userInfo.userLName.charAt(0).toUpperCase();
+  }
+}
+window.onload = function () {
+  showUserIcon();
+};
 
 function sumbitPost() {
   if (title.value.trim() === "" && descr.value.trim() === "") {
-   Swal.fire({
-  icon: "error",
-  title: "Oops...",
-  text: "Title and Description are required!",
-  color: "#ffffff",
-  background: "#1e293b",
-  showConfirmButton: true,
-  confirmButtonText: "OK",
-  confirmButtonColor: "#ef4444", 
-  customClass: {
-    popup: "vibenet-popup",
-    title: "vibenet-title",
-    htmlContainer: "vibenet-content",
-  },
-});
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Title and Description are required!",
+      color: "#ffffff",
+      background: "#1e293b",
+      showConfirmButton: true,
+      confirmButtonText: "OK",
+      confirmButtonColor: "#ef4444",
+      customClass: {
+        popup: "vibenet-popup",
+        title: "vibenet-title",
+        htmlContainer: "vibenet-content",
+      },
+    });
     return;
   } else {
+    var currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+var firstName = currentUser.userFName;
+var lastName = currentUser.userLName;
     post.innerHTML += `<div class="cardWraper">
     <div class="postCard p-4 mb-4 border-0 shadow-sm mb-2" style="background-image: url(${cardBg}); background-attachment: fixed; background-size: cover; background-position: center;">
                 <div class="d-flex align-items-center mb-3">
-                <div class="user-profile-circle me-3 bg-info">${userNAme.charAt(0).toUpperCase()}${userLname.charAt(0).toUpperCase()}</div>
+                <div class="user-profile-circle me-3 bg-info">${firstName.charAt(0).toUpperCase()}${lastName.charAt(0).toUpperCase()}</div>
                 <div>
-                  <h6 class="mb-0 text-white fw-bold">${userNAme.charAt(0).toUpperCase() + userNAme.slice(1)} ${userLname.charAt(0).toUpperCase() + userLname.slice(1)}</h6>
+                  <h6 class="mb-0 text-white fw-bold">${firstName.charAt(0).toUpperCase() + firstName.slice(1)} ${lastName.charAt(0).toUpperCase() + lastName.slice(1)}</h6>
                   <span class="post-meta small color">Posted ${currentTime}</span>
                 </div>
               </div>
@@ -165,6 +241,5 @@ function removeSelected() {
   }
 }
 function logout() {
-  localStorage.removeItem("userName");
   window.location.href = "index.html";
 }
